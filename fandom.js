d7cad6970535b64cb10798179709f6cfda6631ad
Rodrigo <velez59@gmail.com>
2023-10-14 18:21:40 -0700

    feat: added routes

---
+++ fandom.js
@@ -0,0 +1,12 @@
+function letsGoTeam(team) {
+  return `LET'S GO ${team.toUpperCase()}!!!!!!!!`;
+}
+
+function teamSucks(team) {
+  return `THE ${team.toUpperCase()} SUCK!!!!!!!!`;
+}
+
+module.exports = {
+  letsGoTeam,
+  teamSucks,
+}
--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const express = require('express');
 const app = express();
+const { letsGoTeam, teamSucks } = require('./fandom.js');
 const fs = require('fs');
+
+// Set Up Routes
+// Core Routes
+app.get('/read', (req, res) => {
+  let element = req.query.something;
+  fs.readFile(`${element}.txt`, 'utf8', (error, data) => {
+    if (error) {
+      return res.json({
+        message: 'There is an issue, try again later...'
+      });
+    } else {
+      return res.json({
+        message: data
+      });
+    }
+  })
+})
+
+// Local Routes
+app.get('/letsgoteam/:team', (req, res) => {
+  let answer = letsGoTeam(String(req.params.team));
+  return res.json({
+    answer: answer
+  });
+});
+
+app.get('/teamsucks/:team', (req, res) => {
+  let answer = teamSucks(String(req.params.team));
+  return res.json({
+    answer: answer
+  });
+});
+
+// Third-Party Routes
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Welcome to the World Marbles Championship App'
+  });
+})
 
 // Port setup and listener
 const PORT = process.env.PORT || 8000;
